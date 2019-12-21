90252085bee2f800a1c1ab468cb54ed595347008
cruex <raphael.fumfahr@zaflun.com>
2019-12-21 22:34:31 +0100

    add lang files

---
+++ fr-FR.js
@@ -0,0 +1,72 @@
+export default {
+  auth: {
+    login: 'Sign In',
+    register: 'Sign Up',
+  },
+  nav: {
+    home: 'Home',
+  },
+  followUs: 'Follow eXitus On',
+  footer: {
+    menu: {
+      hub: {
+        title: 'Hub',
+      },
+      store: {
+        title: 'Store',
+      },
+      more: {
+        title: 'More',
+      },
+      social: {
+        title: 'Social',
+      },
+      help: {
+        title: 'Help',
+        contact: 'Contact',
+      },
+    },
+    copyright: 'All rights reserved.',
+    created: 'Created with {heart} in Germany',
+    privacy: 'Privacy Policy',
+    terms: 'Terms & Conditions',
+    imprint: 'Imprint',
+  },
+  pre: {
+    headline: 'We are getting ready',
+    sub: 'Subscribe to know more about the first release of eXitus.',
+    signUpEA: 'Sign up for early access',
+    followUs: 'Follow us',
+    features: {
+      ghosts: {
+        title: 'Ghosts',
+        text: 'You have collected Ghosts through our service? You can exchange them for hardware, games or in-game items soon! So don\'t stop making challenges, it will be worth it!',
+      },
+      challenges: {
+        title: 'Challenges',
+        text: 'Get Ghosts through our new Challenge System. Get new challenges, missions or co-op challenges every day. Or at events with a time limit.',
+      },
+      store: {
+        title: 'Store',
+        text: 'Buy premium, games, hardware and in-game items through our store. Pay with Our Ghosts or support our community and pay with one of our many payment providers.',
+      },
+      hubs: {
+        title: 'Hubs',
+        text: 'Our hub system offers you many opportunities to work with our community. There is an area for maps, textures, ideas or questions and much more.',
+      },
+    },
+  },
+  countdown: {
+    days: 'Days',
+    day: 'Day',
+    hours: 'Hours',
+    hour: 'Hour',
+    minutes: 'Minutes',
+    minute: 'Minute',
+    seconds: 'Seconds',
+    second: 'Second',
+  },
+  country: {
+    germany: 'Germany',
+  },
+}
